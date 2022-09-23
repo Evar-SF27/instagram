a3d78160170be6844dfdd4430619db3343044bba
Evar-SF27 <evarfomuso29@gmail.com>
2022-09-23 13:36:07 +0100

    update - 23/09/2022

--- src/components/profile/header.js
+++ src/components/profile/header.js
@@ -5,7 +5,6 @@ import useUser from '../../hooks/use-user';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import SettingsIcon from '@mui/icons-material/Settings';
 import PersonIcon from '@mui/icons-material/Person';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { followUser, isUserFollowingProfile } from '../../services/firebase'
 import 'react-loading-skeleton/dist/skeleton.css';
 import './styles/profile.css'
@@ -52,79 +51,89 @@ export default function Header({
                             className='profile__headerAvatar' 
                         />
                     )}
-                    <div className='profile__info'>
-                        <div className='profile__containerOne'>
-                            <div className='profile__innercontainerOne'>
-                                <p className='profile__username'>{profileUserName}</p>
-                                <p className='optionsOne'>{ activeBtnFollow ? <MoreHorizIcon /> : < SettingsIcon fontSize='small'/> }</p>
-                            </div>
-                            <div className='profile__containerTwo'>
-                                {!activeBtnFollow ? (
-                                    <button 
-                                        className='btn profile__btnNrml'
-                                        type='button'
-                                    >
-                                    Edit Profile
-                                </button>
-                                ) : (
-                                    <>
-                                        { isFollowingProfile && <button className='btn profile__btnNrml' type='button'>
-                                            Message
-                                        </button> }
-                                        {isFollowingProfile ? (
+                    {!profileUserName ? (
+                            <>
+                                <Skeleton count={1} height={20} />
+                                <Skeleton count={1} height={20} />
+                                <Skeleton count={1} height={40} />
+                                <Skeleton count={3} height={10} />
+                            </>
+                        ) : (
+                            <div className='profile__info'>
+                                <div className='profile__containerOne'>
+                                    <div className='profile__innercontainerOne'>
+                                        <p className='profile__username'>{profileUserName}</p>
+                                        <p className='optionsOne'>{ activeBtnFollow ? <MoreHorizIcon /> : < SettingsIcon fontSize='small'/> }</p>
+                                    </div>
+                                    <div className='profile__containerTwo'>
+                                        {!activeBtnFollow ? (
                                             <button 
-                                                className='btn profile__unfollowBtn'
+                                                className='btn profile__btnNrml'
                                                 type='button'
-                                                onClick={handleToggleFollow}
-                                                onKeyDown={(event) => {
-                                                    if (event.key === 'Enter') {
-                                                    handleToggleFollow();
-                                                    }
-                                                }}
                                             >
-                                                <PersonIcon fontSize='small'/>
-                                            </button>
+                                            Edit Profile
+                                        </button>
                                         ) : (
-                                            <button 
-                                                className='btn profile__followBtn'
-                                                type='button'
-                                                onClick={handleToggleFollow}
-                                                onKeyDown={(event) => {
-                                                    if (event.key === 'Enter') {
-                                                    handleToggleFollow();
-                                                    }
-                                                }}
-                                            >
-                                                Follow
-                                            </button>
+                                            <>
+                                                { isFollowingProfile && <button className='btn profile__btnNrml' type='button'>
+                                                    Message
+                                                </button> }
+                                                {isFollowingProfile ? (
+                                                    <button 
+                                                        className='btn profile__unfollowBtn'
+                                                        type='button'
+                                                        onClick={handleToggleFollow}
+                                                        onKeyDown={(event) => {
+                                                            if (event.key === 'Enter') {
+                                                            handleToggleFollow();
+                                                            }
+                                                        }}
+                                                    >
+                                                        <PersonIcon fontSize='small'/>
+                                                    </button>
+                                                ) : (
+                                                    <button 
+                                                        className='btn profile__followBtn'
+                                                        type='button'
+                                                        onClick={handleToggleFollow}
+                                                        onKeyDown={(event) => {
+                                                            if (event.key === 'Enter') {
+                                                            handleToggleFollow();
+                                                            }
+                                                        }}
+                                                    >
+                                                        Follow
+                                                    </button>
+                                                )}
+                                            </>
                                         )}
-                                    </>
-                                )}
+                                    </div>
+                                    <p className='optionsTwo'>{ activeBtnFollow ? <MoreHorizIcon /> : < SettingsIcon /> }</p>
+                                </div>
+                                <div className='profile__containerThree'>
+                                    <div>
+                                        <p className='profile__text_bold'>{photosCount}</p>
+                                        <p className='profile__text_sm'>posts</p>
+                                    </div>
+                                    <div>
+                                        <p className='profile__text_bold'>{followerCount}</p>
+                                        <p className='profile__text_sm'>{followerCount === 1 ? `follower` : `followers`}</p>
+                                    </div>
+                                    <div>
+                                        <p className='profile__text_bold'>{following.length}</p>
+                                        <p className='profile__text_sm'>following</p>
+                                    </div>
+                                </div>
+                                <div className='profile__containerFour'>
+                                    <p className='profile__fullname'>{profileFullName}</p>
+                                    <p className='profile__bio'>Your bio goes in here...</p>
+                                    <div className='profile__followDetails'>
+                                        <p className='text__sm'>Followed by <span className='text__bold'>user_one, user_two, user_three</span> +83 more</p>
+                                    </div>
+                                </div>
                             </div>
-                            <p className='optionsTwo'>{ activeBtnFollow ? <MoreHorizIcon /> : < SettingsIcon /> }</p>
-                        </div>
-                        <div className='profile__containerThree'>
-                            <div>
-                                <p className='profile__text_bold'>{photosCount}</p>
-                                <p className='profile__text_sm'>posts</p>
-                            </div>
-                            <div>
-                                <p className='profile__text_bold'>{followerCount}</p>
-                                <p className='profile__text_sm'>{followerCount === 1 ? `follower` : `followers`}</p>
-                            </div>
-                            <div>
-                                <p className='profile__text_bold'>{following.length}</p>
-                                <p className='profile__text_sm'>following</p>
-                            </div>
-                        </div>
-                        <div className='profile__containerFour'>
-                            <p className='profile__fullname'>{profileFullName}</p>
-                            <p className='profile__bio'>Your bio goes in here...</p>
-                            <div className='profile__followDetails'>
-                                <p className='text__sm'>Followed by <span className='text__bold'>user_one, user_two, user_three</span> +83 more</p>
-                            </div>
-                        </div>
-                    </div>
+                        )}
+                    
                 </div>
                 <div className='profile__description'>
                     <p className='profile__fullname'>{profileFullName}</p>
--- src/components/profile/photos.js
+++ src/components/profile/photos.js
@@ -5,9 +5,61 @@ import 'react-loading-skeleton/dist/skeleton.css';
 
 export default function Photos({ photos }) {
   return (
-    <div>
-      Photos
-    </div>
+    <div className="profile__photos">
+      <div className="profile__photos__container">
+        {!photos
+          ? new Array(12).fill(0).map((_, i) => <Skeleton key={i} width={320} height={400} />)
+          : photos.length > 0
+          ? photos.map((photo) => (
+              <div key={photo.docId} className="photo__relative">
+                <img src={photo.imageSrc} alt={photo.caption} className='profile__photo'/>
+
+                <div className="profile__icons__hover">
+                  <p className="profile__p">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      className="photos__icon"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    <span className='photos__text'>{photo.likes.length}</span>               
+                  </p>
+
+                  <p className="profile__p">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      className="photos__icon"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    <span className='photos__text'>{photo.comments.length}</span>
+                  </p>
+                </div>
+              </div>
+            ))
+          : null}
+      </div>
+
+      {!photos || (photos.length === 0 && <p className="profile__text__sm">No Posts Yet</p>)}
+  </div>
   )
 }
+
+Photos.propTypes = {
+  photos: PropTypes.array
+};
+
+
 
